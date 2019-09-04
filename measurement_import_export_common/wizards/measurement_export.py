2e2e9cb366eb24645a930e571629aa2d4bbaf933
mikevhe18 <viriyananda.michael@gmail.com>
2019-09-04 08:18:31 +0700

    [8.0.1.0.0][measurement_import_export]

---
+++ measurement_import_export_common/wizards/measurement_export.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, models, fields
+import base64
+from datetime import datetime
+from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT
+
+
+class MeasurementExport(models.TransientModel):
+    _name = "measurement.export"
+    _description = "Measurement Export Common Feature"
+
+    name = fields.Char(
+        string="File Name",
+        readonly=True
+    )
+
+    data = fields.Binary(
+        string="File",
+        readonly=True
+    )
+
+    state = fields.Selection(
+        string="State",
+        selection=[
+            ('draft', 'draft'),
+            ('success', 'success'),
+            ('failed', 'failed')
+        ],
+        default="draft"
+    )
+
+    @api.multi
+    def _prepare_column(self):
+        self.ensure_one()
+        return [
+            u"name",
+            u"object",
+            u"item_code",
+            u"item_name",
+            u"value",
+        ]
+
+    @api.multi
+    def _prepare_data_to_export(self, measurement, item):
+        self.ensure_one()
+        result = []
+        result.append(measurement.name)
+        if measurement.object_id.name:
+            result.append(measurement.object_id.name)
+        else:
+            result.append("-")
+        if item.item_type_id.code:
+            result.append(item.item_type_id.code)
+        else:
+            result.append("-")
+        if item.item_type_id.name:
+            result.append(item.item_type_id.name)
+        else:
+            result.append("-")
+        result.append("0")
+        return result
+
+    @api.multi
+    def _export(self):
+        active_model = self.env.context.get("active_model", False)
+        active_ids = self.env.context.get("active_ids", False)
+        obj_measurement = self.env[active_model]
+        measurement_ids = obj_measurement.browse(active_ids)
+
+        columns = self._prepare_column()
+        csv = u','.join(columns)
+        csv += "\n"
+        for measurement in measurement_ids:
+            for item in measurement.item_ids:
+                data = self._prepare_data_to_export(
+                    measurement,
+                    item
+                )
+                csv_row = u'","'.join(data)
+                csv += u"\"{}\"\n".format(csv_row)
+        return csv
+
+    @api.multi
+    def button_export(self):
+        self.ensure_one()
+        data = self._export()
+        if data:
+            date_now = fields.Datetime.now()
+            convert_dt = datetime.strptime(
+                date_now,
+                DEFAULT_SERVER_DATETIME_FORMAT
+            )
+            format = convert_dt.strftime("%d-%m-%Y_%H:%M:%S")
+            vals = {
+                "state": "success",
+                "data": base64.b64encode(data.encode('utf8')),
+                "name": "__export__." + str(format) + ".csv"
+            }
+        else:
+            vals = {
+                "state": "failed",
+            }
+        self.write(vals)
+
+        return {
+            "type": "ir.actions.act_window",
+            "res_model": str(self._model),
+            "view_mode": "form",
+            "view_type": "form",
+            "res_id": self.id,
+            "views": [(False, "form")],
+            "target": "new",
+        }
---
+++ measurement_import_export_common/wizards/measurement_import.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api
+import base64
+from tempfile import TemporaryFile
+import csv
+
+
+class MeasurementImport(models.TransientModel):
+    _name = "measurement.import"
+    _description = "Measurement Import Common Feature"
+
+    data = fields.Binary(
+        string="File",
+        required=True
+    )
+
+    state = fields.Selection(
+        string="State",
+        selection=[
+            ('draft', 'draft'),
+            ('success', 'success'),
+            ('failed', 'failed')
+        ],
+        default="draft"
+    )
+
+    @api.multi
+    def _get_obj_item_name(self):
+        self.ensure_one()
+
+        return False
+
+    @api.multi
+    def _import(self):
+        model_item_name = self._get_obj_item_name()
+        if not model_item_name:
+            return False
+        obj_item = self.env[model_item_name]
+        fileobj = TemporaryFile("w+")
+        fileobj.write(base64.decodestring(self.data))
+        fileobj.seek(0)
+
+        reader = csv.reader(fileobj, delimiter=",")
+        reader.next()
+
+        for row in reader:
+            name = row[0]
+            item_name = row[3]
+            value = row[4]
+
+            criteria = [
+                ("measurement_id.name", "=", name),
+                ("item_type_id.name", "=", item_name)
+            ]
+            item_id = obj_item.search(criteria)
+            if item_id:
+                state = item_id.measurement_id.state
+                if state == "open":
+                    item_type_id = item_id.item_type_id
+                    if item_type_id.question_type == "quantitative":
+                        item_id.write({
+                            "quantitative_value": value
+                        })
+            else:
+                fileobj.close()
+                return False
+
+        fileobj.close()
+
+        return True
+
+    @api.multi
+    def button_import(self):
+        self.ensure_one()
+        data = self._import()
+        if data:
+            vals = {
+                "state": "success",
+            }
+        else:
+            vals = {
+                "state": "failed",
+            }
+        self.write(vals)
+
+        return {
+            "type": "ir.actions.act_window",
+            "res_model": str(self._model),
+            "view_mode": "form",
+            "view_type": "form",
+            "res_id": self.id,
+            "views": [(False, "form")],
+            "target": "new",
+        }
